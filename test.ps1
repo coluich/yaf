7a6a7cf0b8172067988fe66ef7e1b8495e5c2e15
Pietro Bof <87310730+coluich@users.noreply.github.com>
2025-05-22 17:23:50 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,16 +1,23 @@
 $imageUrl = "https://thumbs.dreamstime.com/b/man-screams-fear-horror-pop-art-retro-vector-illustration-103789731.jpg"
 $outputPath = "$env:USERPROFILE\sfondo.jpg"
-# Create a job to download the file
-$job = Start-Job -ScriptBlock {
-    param($url, $path)
-    Invoke-WebRequest -Uri $url -OutFile $path
-} -ArgumentList $imageUrl, $outputPath
-# Wait for the job to complete
-Wait-Job -Job $job | Out-Null
-# Set the wallpaper
+
+# Verifica se l'immagine esiste già
+if (-not (Test-Path -Path $outputPath)) {
+    # Crea un job per scaricare l'immagine solo se non esiste
+    $job = Start-Job -ScriptBlock {
+        param($url, $path)
+        Invoke-WebRequest -Uri $url -OutFile $path
+    } -ArgumentList $imageUrl, $outputPath
+    
+    # Attende il completamento del job
+    Wait-Job -Job $job | Out-Null
+    # Pulisce il job
+    Remove-Job -Job $job
+}
+
+# Imposta lo sfondo (anche se già scaricato in precedenza)
 $RegKeyPath = "HKCU:\Control Panel\Desktop"
 Set-ItemProperty -Path $RegKeyPath -Name Wallpaper -Value $outputPath
+# Aggiorna le impostazioni dello sfondo
 rundll32.exe user32.dll, UpdatePerUserSystemParameters
 RUNDLL32.EXE USER32.DLL, UpdatePerUserSystemParameters 1, True
-# Clean up the job
-Remove-Job -Job $job
